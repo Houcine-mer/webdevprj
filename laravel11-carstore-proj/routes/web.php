900209ee9330c917f8c9f021c72a97b84a362706
Houcine-mer <houcinemer10@gmail.com>
2025-12-03 20:43:18 +0100

    simple changes in the files just for testing

--- laravel11-carstore-proj/routes/web.php
+++ laravel11-carstore-proj/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Newcontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Newcontroller::class, 'welcome' ])->name('welcome');
